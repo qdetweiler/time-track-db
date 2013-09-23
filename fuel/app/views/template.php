3fd963eab9ee9991d47d4f44571fcfd41192a717
Quinn Detweiler <git+qdetweiler@gmail.com>
2013-09-23 09:14:54 -0400

    Added favicon.

--- fuel/app/views/template.php
+++ fuel/app/views/template.php
@@ -17,6 +17,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="Shortcut Icon" href="<?php echo Uri::create('assets/img/favicon.ico')?>" type="image/x-icon"/>
     <title><?php if(isset($title)) echo $title?></title>
 
     <?php echo Asset::css(array('defaultstyle.css',
