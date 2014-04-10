a0a542eb63f6d6058b7c4f3c3560e169efcda167
qdetweiler <qdetweiler@gmail.com>
2014-04-10 10:52:53 -0400

    Updated period totals view to include log group information.

--- fuel/app/views/logs/logtable2_period_totals.php
+++ fuel/app/views/logs/logtable2_period_totals.php
@@ -11,17 +11,31 @@
       <!--    More than one user    -->
       <?php if (count($users) > 1): ?>
 
-        <span class='pt_name'>
-          <h3><?php echo $user['name'] ?></h3>
-        </span>
+        <div class='pt_name'>
+          <h3 class='pt_name_h3'><?php echo $user['name'] ?></h3>
+        </div>
 
       <?php endif?>
 
-      <span class='pt_total_display'>
-        <h3><?php echo $user['total']?></h3>
-      </span>
-    
-    <?php if(count($users) > 1):?>
+      <div class='pt_total_display'>
+        <?php foreach ($user['period_totals'] as $label => $total): ?>
+            <div class='group_display'>  
+                <span class='group_label'><?php echo $label ?>:</span>
+                <span class='group_total'> <?php echo $total ?></span>
+            </div>
+        <?php endforeach ?>
+        <?php if (count($user['period_totals']) > 1): ?>
+            <div id='break_div'><hr id='break'></div>
+            <div class='group_display'>
+                <span class='group_label'>Total:</span>
+                <span class='group_total'><?php echo $user['total'] ?></span>
+            </div>
+        <?php endif ?>
+        <?php if(count($user['period_totals']) == 0):?>
+            <p>None</p>
+        <?php endif?>
+      </div>
+    <?php if(count($users) > 1):?>  
     </div>
     <?php endif?>
     
