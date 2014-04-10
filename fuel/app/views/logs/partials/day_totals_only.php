85718f4c9d5d5870b01910b25a2adcc8e6ace403
qdetweiler <qdetweiler@gmail.com>
2014-04-10 09:46:41 -0400

    Updated day totals view to reflect log type differences.

--- fuel/app/views/logs/partials/day_totals_only.php
+++ fuel/app/views/logs/partials/day_totals_only.php
@@ -6,10 +6,19 @@
 ?>
 <div class='day_display'>
 
-  <div class='day_label'><?php echo $day_label?></div>
-  <div class='log_display'>
-    <span class='log_time'><?php echo($time)?></span>
-  </div>
-  
+    <div class='day_label'><?php echo $day_label ?></div>
+    <?php if ($totals): ?>
+        <?php foreach ($totals as $label => $time): ?>
+            <div class='log_display'>
+                <span class='log_time'><?php echo($label) ?>:</span>
+                <span class='log_time'><?php echo $time ?></span>
+            </div>
+        <?php endforeach ?>
+    <?php else : ?>
+        <div class='log_display'>
+            <p class='log_time grey'>None</p>
+        </div>
+    <?php endif ?>
+
+
 </div>
-    
